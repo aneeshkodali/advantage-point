6bfbe49bb22b265df9e045c4cdfc69a252086c25
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-09-24 01:13:06 -0400

    Change file path for chromedriver.exe

--- utils/functions/selenium_fn.py
+++ utils/functions/selenium_fn.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.service import Service
 def create_webdriver():
     
     # set up driver
-    service = Service(executable_path='./webdriver/chromedriver.exe')
+    service = Service(executable_path='utils/webdriver/chromedriver.exe')
     driver = webdriver.Chrome(service=service)
 
     return driver
