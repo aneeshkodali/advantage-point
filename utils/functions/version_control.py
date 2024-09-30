c1f408d2a8353def63fbdaf21173f23fec63e1ff
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-09-30 01:36:50 -0400

    Add function to return current branch

---
+++ utils/functions/version_control.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def get_current_branch():
+    """
+    Returns branch currently checked out
+    """
+
+    # get the current git branch
+    branch = subprocess.check_output(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+        stderr=subprocess.STDOUT
+    ).strip().decode('utf-8')
+
+    return branch
+
+print(get_current_branch())
