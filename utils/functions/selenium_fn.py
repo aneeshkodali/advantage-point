a3aeea5a8a0bfcdf0a4761e39586dd726c2f287d
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-10-24 22:24:19 -0400

    Make webdriver headless and add path as argument

--- utils/functions/selenium_fn.py
+++ utils/functions/selenium_fn.py
@@ -1,10 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-def create_webdriver():
+def create_webdriver(webdriver_path):
+    """
+    Arguments:
+    - webdriver_path: path to webdriver
+
+    Returns selenium webdriver
+    """
+
+    # set up options for headless mode
+    webdriver_options = Options()
+    webdriver_options.add_argument("--headless")  # run in headless mode
+    webdriver_options.add_argument("--no-sandbox")  # overcome limited resource problems
+    webdriver_options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+
     
-    # set up driver
-    service = Service(executable_path='utils/webdriver/chromedriver.exe')
-    driver = webdriver.Chrome(service=service)
+    # set up webdriver path
+    webdriver_service = Service(executable_path=webdriver_path)
+
+    # create webdriver
+    driver = webdriver.Chrome(
+        service=webdriver_service,
+        options=webdriver_options
+    )
 
     return driver
