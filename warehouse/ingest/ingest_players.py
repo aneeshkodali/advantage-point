cabcb88e60353ca158f5ddf3c80884579fddfcd3
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-10-26 09:55:16 -0400

    Create logic to return player data

--- warehouse/ingest/ingest_players.py
+++ warehouse/ingest/ingest_players.py
@@ -1,4 +1,11 @@
+from typing import (
+    Dict,
+    List
+)
+from utils.functions.selenium_fn import create_chromedriver
 import ast
+import logging
+import os
 import re
 
 def create_player_url(
@@ -22,19 +29,18 @@ def create_player_url(
     return player_url
 
 
-def get_player_list_source(
-    driver,
-    player_list_url: str
+def get_player_url_list_source(
+    driver
 ) -> List:
     """
     Arguments:
     - driver: Selenium webdriver
-    - player_list_url: url that contains list of players
 
     Returns list of player urls from source (url)
     """
 
     # retrieve url page source
+    player_list_url = 'https://www.minorleaguesplits.com/tennisabstract/cgi-bin/jsplayers/mwplayerlist.js'
     driver.get(player_list_url)
     response_page_source = driver.page_source
 
@@ -62,14 +68,80 @@ def get_player_list_source(
         )
         player_url_list.append(player_url)
 
-def get_player_list_ingest():
+    return player_url_list
+
+def fetch_player_data_scraped(
+    driver,
+    player_url: str
+) -> Dict:
     """
-    Returns list of player urls from ingest table
+    Arguments:
+    - driver: Selenium webdriver
+    - player_url: player link
+
+    Returns dictionary of player information from url
     """
-    pass
+
+    # get url page source
+    driver.get(player_url)
+    response_page_source = driver.page_source
+
+    # initialize data to be retrieved
+    data_dict = {}
+    response_var_list = ['fullname', 'lastname', 'currentrank', 'peakrank', 'peakfirst', 'peaklast', 'dob', 'ht', 'hand', 'backhand', 'country', 'shortlist', 'careerjs', 'active', 'lastdate', 'twitter', 'current_dubs', 'peak_dubs', 'peakfirst_dubs', 'liverank', 'chartagg', 'photog', 'photog_credit', 'photog_link', 'itf_id', 'atp_id', 'dc_id', 'wiki_id']
+    for regex_var in response_var_list:
+        regex_pattern = fr"var {regex_var}\s?=\s?(?P<{regex_var}>.*?);"
+        regex_var_match = re.search(regex_pattern, response_page_source)
+        val = regex_var_match.group(regex_var) or None
+        data_dict[regex_var] = val
+
+    return data_dict
 
 def main():
-    pass
+    
+     set logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    # initialize driver
+    webdriver_path = os.getenv('CHROMEDRIVER_PATH')
+    driver = create_chromedriver(webdriver_path=webdriver_path)
+
+    # get list of players
+    player_url_list = get_player_url_list_source(
+        driver=driver,
+        player_list_url='https://www.minorleaguesplits.com/tennisabstract/cgi-bin/jsplayers/mwplayerlist.js'
+    )
+    logging.info(f"Found {len(player_list)} players.")
+
+    # loop through players
+    # initialize list
+    player_data_list = []
+    for i, player_url in enumerate(player_list):
+
+        logging.info(f"Starting {i+1} of {len(player_url_list)}.")
+
+        # initialize player data dictionary
+        player_data_dict = {}
+        player_data_dict['player_url'] = player_url
+
+        # get player data from webscrape
+        player_data_dict_scraped = fetch_player_data_scraped(
+            driver=driver,
+            player_url=player_url
+        )
+
+        # combine dictionaries
+        player_data_dict = {
+            **player_data_dict,
+            **player_data_dict_scraped,
+        }
+
+        player_data_list.append(player_data_dict)
+
+    return player_data_list
 
 if __name__ == "__main__":
     main()
