c6fc685ffdc1d23ebd77c80aae5bcaf60af7b909
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-13 10:15:26 -0500

    Add webdriver options

--- utils/functions/selenium_fn.py
+++ utils/functions/selenium_fn.py
@@ -15,6 +15,9 @@ def create_chromedriver(webdriver_path: str):
     webdriver_options.add_argument("--headless")  # run in headless mode
     webdriver_options.add_argument("--no-sandbox")  # overcome limited resource problems
     webdriver_options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+    webdriver_options.add_argument("--dns-prefetch-disable")  # Disable DNS prefetching to reduce resolution issues
+    webdriver_options.add_argument("--disable-gpu")  # Optional, sometimes helpful in headless mode
+
 
     
     # set up webdriver path
