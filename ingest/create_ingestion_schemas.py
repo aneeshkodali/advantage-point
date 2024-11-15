e8e255bef32d02c2a790ae7d1a3f850ea6ada1c7
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-15 17:18:39 -0500

    Create script to create ingestion schemas

---
+++ ingest/create_ingestion_schemas.py
@@ -0,0 +1,19 @@
+from utils.functions.sql import (
+    create_connection,
+    create_schema
+)
+import os
+
+# get schemas
+schema_ingest = os.getenv('SCHEMA_INGESTION')
+schema_ingest_temp = os.getenv('SCHEMA_INGESTION_TEMP')
+
+# create connection
+conn = create_connection()
+
+# create schemas
+for schema in [schema_ingest, schema_ingest_temp]:
+    create_schema(
+        connection=conn,
+        schema_name=schema
+    )
