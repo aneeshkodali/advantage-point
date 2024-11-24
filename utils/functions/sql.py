c0087a4752cb3336f95b2eda361aeae64fd8c423
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-23 19:33:40 -0500

    Change logging.error to logging.info

--- utils/functions/sql.py
+++ utils/functions/sql.py
@@ -92,9 +92,9 @@ def get_table_column_list(
     column_name_join = ', '.join(column_name_list)
     where_clause_join = ' AND '.join([f"({where_clause})" for where_clause in where_clause_list])
 
-    # create sql statement to retrieve distinct records for columns
+    # create sql statement to retrieve records for columns
     select_sql = f"""
-        SELECT DISTINCT
+        SELECT
             {column_name_join}
         FROM {schema_name}.{table_name}
         WHERE {where_clause_join}
@@ -111,7 +111,7 @@ def get_table_column_list(
         return select_list
 
     except Exception as e:
-        logging.error(f"Error executing statement: {e}")
+        logging.info(f"Error executing statement: {e}")
         return []
 
 def infer_sql_type(python_dtype: Any):
