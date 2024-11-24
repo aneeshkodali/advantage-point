1fdcb00d79b96daed22bc8ce956f994230f2fe30
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-24 13:02:01 -0500

    Get script text

--- ingest/ingest_players_tennisabstract.py
+++ ingest/ingest_players_tennisabstract.py
@@ -134,14 +134,17 @@ def fetch_player_tennisabstract_data_scraped(
         driver.get(player_url)
         response_page_source = driver.page_source
         soup = BeautifulSoup(response_page_source, 'html.parser')
-        page_text = soup.get_text()
-        logging.info(f"page text: {page_text}")
+
+        # find the script tag
+        script_tag = soup.find('script', attrs={'language': 'JavaScript'})
+        script_text = script_tag.text
+        logging.info(f"script text: {script_text}")
 
         # loop through variable list and add values to dict
         for regex_var in response_var_list:
             regex_pattern = fr"var {regex_var}\s?=\s?(?P<{regex_var}>.*);"
             logging.info(f"regex pattern: {regex_pattern}")
-            regex_var_match = re.search(regex_pattern, page_text)
+            regex_var_match = re.search(regex_pattern, script_text)
             logging.info(f"regex var match: {regex_var_match}")
             if regex_var_match:
                 val = regex_var_match.group(regex_var)
