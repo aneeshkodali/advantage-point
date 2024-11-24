c153ed6d0c607c4e74b8d6a0ce5ade78b0885492
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-23 20:22:32 -0500

    Replace page source parsing

--- ingest/ingest_players_tennisabstract.py
+++ ingest/ingest_players_tennisabstract.py
@@ -102,14 +102,22 @@ def fetch_player_tennisabstract_data_scraped(
     # initialize data to be retrieved
     data_dict = {}
     response_var_list = ['fullname', 'lastname', 'currentrank', 'peakrank', 'peakfirst', 'peaklast', 'dob', 'ht', 'hand', 'backhand', 'country', 'shortlist', 'careerjs', 'active', 'lastdate', 'twitter', 'current_dubs', 'peak_dubs', 'peakfirst_dubs', 'liverank', 'chartagg', 'photog', 'photog_credit', 'photog_link', 'itf_id', 'atp_id', 'dc_id', 'wiki_id']
-    for regex_var in response_var_list:
-        regex_pattern = fr"var {regex_var}\s?=\s?(?P<{regex_var}>.*?);"
-        regex_var_match = re.search(regex_pattern, response_page_source)
-        if regex_var_match:
-            val = regex_var_match.group(regex_var)
-        else:
-            val = None
-        data_dict[regex_var] = val
+    # for regex_var in response_var_list:
+    #     regex_pattern = fr"var {regex_var}\s?=\s?(?P<{regex_var}>.*?);"
+    #     regex_var_match = re.search(regex_pattern, response_page_source)
+    #     if regex_var_match:
+    #         val = regex_var_match.group(regex_var)
+    #     else:
+    #         val = None
+    #     data_dict[regex_var] = val
+    # Use JavaScript execution to retrieve variables
+    for var_name in response_var_list:
+        try:
+            js_script = f"return typeof {var_name} !== 'undefined' ? {var_name} : null;"
+            val = driver.execute_script(js_script)
+        except Exception as e:
+            val = None  # Handle variables not found or errors in JS execution
+        data_dict[var_name] = val
 
     return data_dict
 
