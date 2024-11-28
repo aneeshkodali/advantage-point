f5aa05f79a4a563db6c48e5902e902afa7e98559
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-28 01:42:22 -0500

    Remove limit on records

--- ingest/ingest_players_tennisabstract.py
+++ ingest/ingest_players_tennisabstract.py
@@ -78,9 +78,6 @@ def get_player_tennisabstract_url_list_source(
     player_url_list = []
     for player in player_list:
 
-        if ('Seppi' not in player) and ('William' not in player):
-            continue
-
         # each element in list is of format: (<gender>) <name>)
         regex_pattern = r'(?P<gender>\((.*?)\))\s*(?P<name>.*)'
         regex_match = re.search(regex_pattern, player)
@@ -201,7 +198,7 @@ def main():
     #     where_clause_list=['audit_field_active_flag = TRUE']
     # )
     # player_tennisabstract_url_list = list(filter(lambda url_dict: url_dict not in player_tennisabstract_url_list_db, player_tennisabstract_url_list_source))
-    player_tennisabstract_url_list = player_tennisabstract_url_list_source[:30]
+    player_tennisabstract_url_list = player_tennisabstract_url_list_source
     logging.info(f"Found {len(player_tennisabstract_url_list)} players.")
 
     # loop through players
