59251e905c3561ed2df72a0871e7deaae27392aa
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-28 00:23:17 -0500

    Import By

--- ingest/ingest_players_tennisabstract.py
+++ ingest/ingest_players_tennisabstract.py
@@ -4,6 +4,7 @@ from typing import (
     List
 )
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from utils.functions.sql import (
