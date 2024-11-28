199235b1571acecdd2dd087fba98f5d7952f3620
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-27 23:10:18 -0500

    Adjust scraping page source function

--- utils/functions/scrape.py
+++ utils/functions/scrape.py
@@ -14,8 +14,9 @@ def scrape_page_source_var(
     """
 
     # within page source, variable is of value: var {var}{optional space}={optional space}{value};
-    regex_pattern = fr"var {var}\s?=\s?(?P<{var}>.*);"
-    regex_var_match = re.search(regex_pattern, page_source)
+    # regex_pattern = fr"var {var}\s?=\s?(?P<{var}>.*);"
+    regex_pattern = fr"var {var}\s?=\s?(?P<{var}>.*?)(?=;)$"
+    regex_var_match = re.search(regex_pattern, page_source, re.MULTILINE)
     val = regex_var_match.group(var) if regex_var_match else None
 
     return val
