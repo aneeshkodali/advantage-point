8ac3e1daeb4ba9bfe4d1dab4baebff87d3413ad8
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-29 18:51:24 -0500

    Fix import reference

--- ingest/create_ingestion_schemas.py
+++ ingest/create_ingestion_schemas.py
@@ -1,29 +1,34 @@
-from utils.functions.sql import (
+from ingest.utils.functions.sql import (
     create_connection,
     create_schema
 )
 import logging
 import os
 
-# set logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+def main():
 
-# get schemas
-schema_ingest = os.getenv('SCHEMA_INGESTION')
-schema_ingest_temp = os.getenv('SCHEMA_INGESTION_TEMP')
-
-# create connection
-conn = create_connection()
-
-# create schemas
-for schema in [schema_ingest, schema_ingest_temp]:
-    create_schema(
-        connection=conn,
-        schema_name=schema
+    # set logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-# close connection
-conn.close()
+    # get schemas
+    schema_ingest = os.getenv('SCHEMA_INGESTION')
+    schema_ingest_temp = os.getenv('SCHEMA_INGESTION_TEMP')
+
+    # create connection
+    conn = create_connection()
+
+    # create schemas
+    for schema in [schema_ingest, schema_ingest_temp]:
+        create_schema(
+            connection=conn,
+            schema_name=schema
+        )
+
+    # close connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
