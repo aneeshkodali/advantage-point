c5514c9bbab4fa06096a6713abdadafd222e27fb
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-29 14:09:50 -0500

    Debugging

--- ingest/utils/functions/tennisabstract.py
+++ ingest/utils/functions/tennisabstract.py
@@ -227,7 +227,7 @@ def get_match_point_data(
 
                 # loop through tr list
                 for index, tr in enumerate(pointlog_tr_list):
-                    # logging.info(f"Point {index+1} out of {len(pointlog_tr_list)}")
+                    logging.info(f"Point {index+1} out of {len(pointlog_tr_list)}")
                     tr_td_list = tr.find_all('td')
                     point_data = {
                         'match_url': match_url,
