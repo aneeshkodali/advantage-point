720ebbc15d487f2f30e40f6572d310a1113b3d32
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-29 12:28:12 -0500

    Rename variable

--- ingest/ingest_tennisabstract_matches.py
+++ ingest/ingest_tennisabstract_matches.py
@@ -39,14 +39,14 @@ def main():
     match_url_list_tennisabstract = get_match_url_list_tennisabstract(
         # driver=driver
     )
-    match_tennisabstract_url_list_db = get_table_column_list(
+    match_url_list_db = get_table_column_list(
         connection=conn,
         schema_name=target_schema_name,
         table_name=target_table_name,
         column_name_list=unique_column_list,
         where_clause_list=['audit_field_active_flag = TRUE',]
     )
-    match_url_list = list(filter(lambda url_dict: url_dict not in match_tennisabstract_url_list_db, match_url_list_tennisabstract))
+    match_url_list = list(filter(lambda url_dict: url_dict not in match_url_list_db, match_url_list_tennisabstract))
     logging.info(f"Found {len(match_url_list)} matches.")
 
     # loop through matches
