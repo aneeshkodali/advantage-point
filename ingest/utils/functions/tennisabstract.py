58b4bb6f9cd986f2ac3357c764857ccb8e245072
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-11-29 11:42:51 -0500

    Debugging

--- ingest/utils/functions/tennisabstract.py
+++ ingest/utils/functions/tennisabstract.py
@@ -104,7 +104,11 @@ def get_match_data_scraped(
             response = make_request(url=match_url)
             logging.info(f"response: {response.text[:500]}")
             soup = BeautifulSoup(response.text, 'html.parser')
-            logging.info(f"soup: {soup[:500]}")
+            # logging.info(f"soup: {soup[:500]}")
+            logging.info(f"title tag: {soup.find('title').text}")
+            logging.info(f"match title: {soup.find('h2').text}")
+            logging.info(f"match title regex: {soup.find('h2', string=re.compile(r".+:\s*.+\s+vs\s+.+")).text}")
+                
 
             # wait for the page to fully render
             # WebDriverWait(driver, 10).until(
@@ -113,8 +117,6 @@ def get_match_data_scraped(
 
             # get the match title (<h2>): <match info>: <player1> vs <player2>
             try:
-                logging.info(f"match title: {soup.find('h2').text}")
-                logging.info(f"match title regex: {soup.find('h2', string=re.compile(r".+:\s*.+\s+vs\s+.+")).text}")
                 match_title = soup.find('h2', string=re.compile(r".+:\s*.+\s+vs\s+.+")).text
             except Exception as e:
                 logging.info(f"Error encountered when getting data for variable match_title: {e}")
