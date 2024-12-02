a753c559fe8f756734307105e039ea2c0dd9636c
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-02 15:30:04 -0500

    Replace dash with nonbreaking hyphen

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -4,6 +4,10 @@ source as (
     select * from {{ source('tennisabstract', 'match_points') }}
 ),
 
+/*
+note:
+'‑' is a non-breaking hyphen
+*/
 renamed as (
     select
         match_url,
@@ -11,19 +15,19 @@ renamed as (
         -- sets
         "server" as point_server,
         "sets" as set_score,
-        cast(split_part("sets", '-', 1) as int) as set_score_server,
-        cast(split_part("sets", '-', 2) as int) as set_score_receiver,
+        cast(split_part("sets", '‑', 1) as int) as set_score_server,
+        cast(split_part("sets", '‑', 2) as int) as set_score_receiver,
 
         -- games
         games as game_score,
-        cast(split_part(games, '-', 1) as int) as game_score_server,
-        cast(split_part(games, '-', 2) as int) as game_score_receiver,
+        cast(split_part(games, '‑', 1) as int) as game_score_server,
+        cast(split_part(games, '‑', 2) as int) as game_score_receiver,
 
         -- points
         cast(point_number as int) as point_in_match,
         points as point_score,
-        split_part(points, '-', 1) as point_score_server,
-        split_part(points, '-', 2) as point_score_receiver,
+        split_part(points, '‑', 1) as point_score_server,
+        split_part(points, '‑', 2) as point_score_receiver,
         point_description,
         
         -- audit fields
