6f2378fb122a79aad651b2b5e83429f59d57f34b
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-02 07:46:45 -0500

    Create stg_tennisabstract__match_points model

---
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -0,0 +1,41 @@
+with
+
+source as (
+    select * from {{ source('tennisabstract', 'match_points') }}
+),
+
+renamed as (
+    select
+        match_url,
+
+        -- sets
+        `server` as point_server,
+        `sets` as set_score,
+        cast(split_part(`server`, '-', 1) as int) as set_score_server,
+        cast(split_part(`server`, '-', 2) as int) as set_score_receiver,
+        set_score_server + set_score_receiver + 1 as set_in_match,
+
+        -- games
+        games as game_score,
+        cast(split_part(games, '-', 1) as int) as game_score_server,
+        cast(split_part(games, '-', 2) as int) as game_score_receiver,
+        game_score_server + game_score_receiver + 1 as game_in_set,
+        row_number() over (partition by match_url order by set_in_match, game_in_set) as game_in_match
+
+        -- points
+        cast(point_number as int) as point_in_match,
+        row_number() over (partition by match_url, set_score, game_score order by point_in_match) as point_in_game,
+        row_number() over (partition by match_url, set_score order by point_in_match) as point_in_set,
+        points as point_score,
+        split_part(points, '-', 1) as point_score_server,
+        split_part(points, '-', 2) as point_score_receiver,
+        point_description,
+        
+        -- audit fields
+        audit_field_active_flag as active,
+        audit_field_start_datetime_utc as loaded_at
+    from source
+
+)
+
+select * from renamed
