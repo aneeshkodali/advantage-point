fdd6b97e347aaede0283d9a25a3f5e1c410cd590
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-02 15:20:45 -0500

    Remove window function columns from model

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -18,7 +18,7 @@ renamed as (
         games as game_score,
         cast(split_part(games, '-', 1) as int) as game_score_server,
         cast(split_part(games, '-', 2) as int) as game_score_receiver,
-        
+
         -- points
         cast(point_number as int) as point_in_match,
         points as point_score,
@@ -28,19 +28,15 @@ renamed as (
         
         -- audit fields
         audit_field_active_flag as active,
-        audit_field_start_datetime_utc as loaded_at,
+        audit_field_start_datetime_utc as loaded_at
     from source
-
 ),
 
 processed as (
     select
         *,
         set_score_server + set_score_receiver + 1 as set_in_match,
-        game_score_server + game_score_receiver + 1 as game_in_set,
-        row_number() over (partition by match_url order by set_in_match, game_in_set) as game_in_match,
-        row_number() over (partition by match_url, set_score, game_score order by point_in_match) as point_in_game,
-        row_number() over (partition by match_url, set_score order by point_in_match) as point_in_set,
+        game_score_server + game_score_receiver + 1 as game_in_set
     from renamed
 )
 
