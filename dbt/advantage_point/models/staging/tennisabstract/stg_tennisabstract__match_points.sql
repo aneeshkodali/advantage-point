052847ac928be01b74dd71b840793eef4d813656
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-05 00:43:40 -0500

    Remove intermediate transformations

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -11,26 +11,12 @@ note:
 renamed as (
     select
         match_url,
-
-        -- sets
+        cast(point_number as int) as point_in_match,
         "server" as point_server,
         "sets" as set_score,
-        cast(split_part("sets", '‑', 1) as int) as set_score_server,
-        cast(split_part("sets", '‑', 2) as int) as set_score_receiver,
-
-        -- games
         games as game_score,
-        cast(split_part(games, '‑', 1) as int) as game_score_server,
-        cast(split_part(games, '‑', 2) as int) as game_score_receiver,
-
-        -- points
-        cast(point_number as int) as point_in_match,
         points as point_score,
-        split_part(points, '‑', 1) as point_score_server,
-        split_part(points, '‑', 2) as point_score_receiver,
         point_description,
-        
-        -- audit fields
         audit_field_active_flag as active,
         audit_field_start_datetime_utc as loaded_at
     from source
