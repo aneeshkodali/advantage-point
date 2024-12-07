d64c709b938c49afe7843df502f9a5f74f661ef0
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-06 22:13:22 -0500

    Change structure of source and stg

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -13,11 +13,11 @@ renamed as (
         match_url,
         cast(point_number as int) as point_in_match,
         "server" as point_server,
-        "sets" as set_score,
-        games as game_score,
-        points as point_score,
+        "sets" as set_score_in_match,
+        games as game_score_in_set,
+        points as point_score_in_game,
         point_description,
-        audit_field_active_flag as active,
+        audit_field_active_flag as is_record_active,
         audit_field_start_datetime_utc as loaded_at
     from source
 )
