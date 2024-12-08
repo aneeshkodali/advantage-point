d624ee3d1702b82d1646298c5829f361840fd811
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-08 10:18:59 -0500

    Add stg_tennisabstract__players model

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__players.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__players.sql
@@ -12,10 +12,10 @@ renamed as (
         {{ convert_rank_to_integer('current_dubs') }} as current_doubles_rank,
         replace(atp_id, "'", "") as tour_id,
         case player_gender
-            when 'M' then 'ATP'
-            when 'W' then 'WTA'
+            when 'M' then 'atp_id'
+            when 'W' then 'wta_id'
             else null
-        end as tour_name,
+        end as tour_id_type,
         replace(photog_link, "'", "") as photograph_link,
         replace(fullname, "'", "") as full_name,
         replace(twitter, "'", "") as twitter_handle,
@@ -40,11 +40,16 @@ renamed as (
         to_date(peaklast, 'YYYYMMDD') as last_peak_singles_rank_on,
         replace(dc_id, "'", "") as team_cup_id,
         case player_gender
-            when 'M' then 'Davis Cup'
-            when 'W' then 'Billie Jean King Cup'
+            when 'M' then 'dc_id'
+            when 'W' then 'bjkc_id'
             else null
-        end as team_cup_name,
+        end as team_cup_id_type,
         replace(lastname, "'", "") as last_name,
+        to_date(dob, 'YYYYMMDD') as date_of_birth,
+        cast(ht as int) as height_in_centimeters
+        {{ convert_rank_to_integer('peakrank') }} as peak_singles_rank,
+        replace(itf_id, "'", "") as itf_id,
+        replace(wiki_id, "'", "") as wikipedia_id,
         audit_field_active_flag as is_record_active,
         audit_field_start_datetime_utc as loaded_at
     from source
