b7c9b90c915d9f39b6b8521d56b8afd66e00cdec
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-11 02:08:00 -0500

    Change macro equal condition

--- dbt/advantage_point/macros/remove_empty_string_from_source.sql
+++ dbt/advantage_point/macros/remove_empty_string_from_source.sql
@@ -1,6 +1,6 @@
 {% macro remove_empty_string_from_source(column_name) %}
-    case replace({{ column_name }}, '''', '')
-            when '' then null
-            else replace({{ column_name }}, '''', '')
-        end
+    case
+        when replace({{ column_name }}, '''', '') = '' then null
+        else replace({{ column_name }}, '''', '')
+    end
 {% endmacro %}
