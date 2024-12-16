4e797deebb0143e415004737a451e3a0b71c7ab5
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-16 07:05:27 -0500

    Add int_sets model

---
+++ dbt/advantage_point/models/intermediate/int_sets.sql
@@ -0,0 +1,32 @@
+with
+
+tennisabstract_match_points as (
+    select
+        *,
+        cast(split_part("sets", '‑', 1) as int) as set_score_server,
+        cast(split_part("sets", '‑', 2) as int) as set_score_receiver
+    from {{ ref('stg_tennisabstract__match_points') }}
+    where is_record_active = true
+),
+
+-- note '‑' is a special character
+match_sets as (
+    select
+        match_url,
+        set_score_in_match,
+        set_score_server,
+        set_score_receiver,
+        set_score_server + set_score_receiver + 1 as set_number_in_match,
+        row_number() over (partition by match_url order by set_score_server + set_score_receiver) as set_number_in_match_window
+    from tennisabstract_match_points
+    group by all
+),
+
+final as (
+    select
+        match_url,
+        coalesce(set_number_in_match, set_number_in_match_window) as set_number_in_match
+    from match_sets
+)
+
+select * from final
