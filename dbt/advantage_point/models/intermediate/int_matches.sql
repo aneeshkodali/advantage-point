c5bbd93a58afd48fcc6e5dd03219bc719c6fdba8
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-16 07:49:35 -0500

    Add int_matches model

--- dbt/advantage_point/models/intermediate/int_matches.sql
+++ dbt/advantage_point/models/intermediate/int_matches.sql
@@ -11,8 +11,27 @@ matches as (
         match_tournament,
         match_date,
         match_round,
-        match_title
+        match_title,
+        concat(
+            year(match_date) || ' ',
+            match_tournament || ' ',
+            match_round || ': ',
+            match_player_one,
+            ' vs ',
+            match_player_two
+        ) as match_title_concat
     from tennis_abstract_matches
+),
+
+final as (
+    select
+        match_url,
+        match_gender,
+        match_tournament,
+        match_date,
+        match_round,
+        coalesce(match_title, match_title_concat) as match_title
+    from matches
 )
 
-select * from matches
+select * from final
