bdbdfa881c08d0d52d7a3e31a6277a41fa65cc98
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-17 23:19:27 -0500

    Add valid_match_unit_outcomes test

---
+++ dbt/advantage_point/tests/valid_match_unit_outcomes.sql
@@ -0,0 +1,10 @@
+with boolean_agg as (
+    select
+        {{ group_by_columns | join(', ') }},
+        sum(case when {{ boolean_column }} = true then 1 else 0 end) as boolean_agg
+    from {{ ref(model) }}
+    group by {{ group_by_columns | join(', ') }}
+)
+select *
+from boolean_agg
+where boolean_agg != 1
