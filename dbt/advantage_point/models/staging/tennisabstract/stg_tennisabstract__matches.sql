de4900936d3958f9481df56f5187b0f49998548a
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-18 00:00:41 -0500

    Add match outcome columns

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__matches.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__matches.sql
@@ -19,10 +19,37 @@ renamed as (
         replace(source.match_player_two, '_', ' ') as match_player_two,
         source.match_title,
         source.match_result,
+
+        -- {winner} d. {loser} {set score}
+        split_part(source.match_result, ' d. ', 1) as match_winner,
+        case
+            when match_winner = match_player_one then match_player_two
+            when match_winner = match_player_two then match_player_one
+            else null
+        end as match_loser,
         source.audit_field_active_flag as is_record_active,
         source.audit_field_start_datetime_utc as loaded_at
     from source
     left join seed_valid_match_dates on source.match_url = seed_valid_match_dates.match_url
+),
+
+final as (
+    select
+        match_url,
+        match_date,
+        match_gender,
+        match_tournament,
+        match_round,
+        match_player_one,
+        match_player_two,
+        match_title,
+        match_result,
+        match_winner,
+        match_loser,
+        split_part(match_result, match_loser || ' ', 2) as match_score,
+        is_record_active,
+        loaded_at
+    from renamed
 )
 
-select * from renamed
+select * from final
