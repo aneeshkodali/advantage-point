d0463ccd4f18f59a9c3df54e69747bdfcdd6d4f4
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-18 01:31:29 -0500

    Add test syntax

--- dbt/advantage_point/tests/valid_match_unit_outcomes.sql
+++ dbt/advantage_point/tests/valid_match_unit_outcomes.sql
@@ -1,10 +1,14 @@
+{% test valid_match_unit_outcomes(model, group_by_columns, boolean_column) %}
+
 with boolean_agg as (
     select
         {{ group_by_columns | join(', ') }},
         sum(case when {{ boolean_column }} = true then 1 else 0 end) as boolean_agg
-    from {{ ref(model.name) }}
+    from {{ model }}
     group by {{ group_by_columns | join(', ') }}
 )
 select *
 from boolean_agg
 where boolean_agg != 1
+
+{% endtest %}
