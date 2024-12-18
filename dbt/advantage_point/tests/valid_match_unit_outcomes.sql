2d832ca5f25a5df1aab378fb0a4d4b2ae5a07c93
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-18 00:12:36 -0500

    Add model.name property

--- dbt/advantage_point/tests/valid_match_unit_outcomes.sql
+++ dbt/advantage_point/tests/valid_match_unit_outcomes.sql
@@ -2,7 +2,7 @@ with boolean_agg as (
     select
         {{ group_by_columns | join(', ') }},
         sum(case when {{ boolean_column }} = true then 1 else 0 end) as boolean_agg
-    from {{ ref(model) }}
+    from {{ ref(model.name) }}
     group by {{ group_by_columns | join(', ') }}
 )
 select *
