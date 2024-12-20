239493e64f7689de96c3e4a760f2ca6e3e6e437c
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-20 12:29:12 -0500

    Add final cte

--- dbt/advantage_point/models/intermediate/tennisabstract/int_tennisabstract__sets.sql
+++ dbt/advantage_point/models/intermediate/tennisabstract/int_tennisabstract__sets.sql
@@ -1,20 +1,32 @@
 with
 
--- note '‑' is a special character
-match_points as (
-    select
-        *,
-        cast(split_part(set_score_in_match, '‑', 1) as int) as set_score_server,
-        cast(split_part(set_score_in_match, '‑', 2) as int) as set_score_receiver
-    from {{ ref('stg_tennisabstract__match_points') }}
-    where is_record_active = true
+matches as (
+    select * from {{ ref('int_tennisabstract__matches') }}
+    where match_score is not null
 ),
 
-match_sets as (
-    select distinct
+-- split sets into own line
+match_sets_unnest as (
+    select
         match_url,
-        set_score_server + set_score_receiver + 1 as set_number_in_match
-    from match_points
+        UNNEST(STRING_TO_ARRAY(match_score, ' ')) AS set_score
+    from matches
+),
+
+-- get set number
+match_sets_row_number as (
+    select
+        match_url,
+        set_score,
+        row_number() over (partition by match_url order by set_score) as set_number_in_match
+    from match_sets_unnest
+),
+
+final as (
+    select
+        match_url,
+        set_number_in_match
+    from match_sets_row_number
 )
 
-select * from match_sets
+select * from final
