a5012efa79f8d25ecc64ebb48746f606fe978ccb
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-24 12:00:15 -0500

    Replace non-breaking space

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -7,12 +7,13 @@ source as (
 /*
 note:
 '‑' is a non-breaking hyphen
+' ' is a non-breaking space
 */
 renamed as (
     select
         match_url,
         cast(point_number as int) as point_number_in_match,
-        "server" as point_server,
+        replace("server", ' ', ' ') as point_server,
         replace("sets", '‑', '-') as set_score_in_match,
         replace(games, '‑', '-') as game_score_in_set,
         replace(points, '‑', '-') as point_score_in_game,
