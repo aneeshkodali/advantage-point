91179241f8704a5f8bf003ca2df480135f5e05c0
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-24 11:42:14 -0500

    Add replace to remove non-breaking hyphen

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__match_points.sql
@@ -4,14 +4,18 @@ source as (
     select * from {{ source('tennisabstract', 'match_points') }}
 ),
 
+/*
+note:
+'‑' is a non-breaking hyphen
+*/
 renamed as (
     select
         match_url,
         cast(point_number as int) as point_number_in_match,
         "server" as point_server,
-        "sets" as set_score_in_match,
-        games as game_score_in_set,
-        points as point_score_in_game,
+        replace("sets", '‑', '-') as set_score_in_match,
+        replace(games, '‑', '-') as game_score_in_set,
+        replace(points, '‑', '-') as point_score_in_game,
         point_description,
         audit_field_active_flag as is_record_active,
         audit_field_start_datetime_utc as loaded_at
