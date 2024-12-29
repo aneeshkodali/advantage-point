bc169861621c2c97e86efb8c0f4534ff0e2ae61d
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-29 09:53:22 -0500

    Add stg_seed__match_rounds model

---
+++ dbt/advantage_point/models/staging/seed/stg_seed__match_rounds.sql
@@ -0,0 +1,14 @@
+with
+
+source as (
+    select * from {{ ref('match_rounds') }}
+),
+
+renamed as (
+    select
+        match_round,
+        match_round_name,
+        cast(match_round_sort as int) as match_round_sort
+)
+
+select * from renamed
