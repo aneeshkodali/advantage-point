f1d17ac83a335b6f1fce27fcfdf5c8fcf064a17c
Aneesh Kodali <aneesh.kodali@gmail.com>
2024-12-31 07:53:43 -0500

    Add logic to get point winner/loser based on next point score

--- dbt/advantage_point/models/intermediate/tennisabstract/int_tennisabstract__matches_points.sql
+++ dbt/advantage_point/models/intermediate/tennisabstract/int_tennisabstract__matches_points.sql
@@ -127,7 +127,22 @@ match_points_outcome as (
   from match_points_last_shot_count
 ),
 
--- get point winner
+-- get rally length
+match_points_rally_length as (
+  select
+    *,
+    -- calculate rally length
+    case
+      -- exclude 'errors'
+      when point_result in ('double fault', 'forced error', 'unforced error') then number_of_shots_in_point - 1
+      -- include 'winners'
+      when point_result in ('ace', 'service winner', 'winner') then number_of_shots_in_point
+      else null
+    end as rally_length
+  from match_points_outcome
+),
+
+-- get point winner from rally
 match_points_winner as (
   select
     *,
@@ -147,37 +162,185 @@ match_points_winner as (
           else null
         end
       else null
-    end as point_winner
+    end as point_winner_rally
     
-  from match_points_outcome
-),
-
--- get rally length
-match_points_rally_length as (
-  select
-    *,
-    -- calculate rally length
-    case
-      -- exclude 'errors'
-      when point_result in ('double fault', 'forced error', 'unforced error') then number_of_shots_in_point - 1
-      -- include 'winners'
-      when point_result in ('ace', 'service winner', 'winner') then number_of_shots_in_point
-      else null
-    end as rally_length
-  from match_points_winner
-),
-
--- get point loser
-match_points_loser as (
-  select
-    *,
-    case
-      when point_winner = point_server then point_receiver
-      when point_winner != point_server then point_server
-      else null
-    end as point_loser
   from match_points_rally_length
 ),
+
+-- get point loser from rally
+match_points_loser_rally as (
+  select
+    *,
+    case
+      when point_winner_rally = point_server then point_receiver
+      when point_winner_rally != point_server then point_server
+      else null
+    end as point_loser_rally
+  from point_winner_rally
+),
+
+-------------------------------------------------------
+-- this next section will get the point winner and loser based on the score in the next point
+
+-- get the NEXT point record
+-- convert point scores to int
+match_points_lead as (
+  select
+    mp.*,
+
+    -- convert 'AD' to numeric
+    cast(replace(mp.point_score_server, 'AD', '41') as int) as point_score_server_int,
+    cast(replace(mp.point_score_receiver, 'AD', '41') as int) as point_score_receiver_int,
+
+    mp_next.point_number_in_match as next_point_point_number_in_match,
+    mp_next.point_server as next_point_point_server,
+    mp_next.point_receiver as next_point_point_receiver,
+    -- convert 'AD' to numeric
+    cast(replace(mp_next.point_score_server, 'AD', '41') as int) as next_point_point_score_server_int,
+    cast(replace(mp_next.point_score_receiver, 'AD', '41') as int) as next_point_point_score_receiver_int,
+    
+    mp_next.game_number_in_match as next_point_game_number_in_match,
+    mp_next.game_score_server as next_point_game_score_server,
+    mp_next.game_score_receiver as next_point_game_score_receiver,
+    
+    mp_next.set_number_in_match as next_point_set_number_in_match,
+    mp_next.set_score_server as next_point_set_score_server,
+    mp_next.set_score_receiver as next_point_set_score_receiver
+
+  from match_points_loser_rally as mp
+  left join match_points_loser_rally as mp_next on
+      mp.match_url = mp_next.match_url
+      and mp.point_number_in_match + 1 = mp_next.point_number_in_match
+),
+
+-- determine point winner based on next point
+match_points_point_winner_next_point as (
+  select
+    *,
+    
+    case
+      -- check if point is last point in match
+      when point_number_in_match = max(point_number_in_match) over (partition by match_url) then
+        -- compare server score and receiver score
+        case
+          -- when server score higher --> server
+          when point_score_server_int > point_score_receiver_int then point_server
+          -- when receiver score higher --> receiver
+          when point_score_server_int < point_score_receiver_int then point_receiver
+          else null
+        end
+
+      -- compare current point game to next point game
+      -- next point is in same game as current point --> compare 'point level' data
+      when game_number_in_match = next_point_game_number_in_match then
+        -- compare current server to next point server
+        case
+          -- next point server is same as current point server
+          when point_server = next_point_point_server then
+            -- compare current point server score to next point server score
+            case
+              -- next server point score is higher --> server won point
+              when point_score_server_int < next_point_point_score_server_int then point_server
+              -- next server point score is equal --> compare receiver point score in current point and next point
+              when point_score_server_int = next_point_point_score_server_int then
+                -- compare current point receiver score to next point receiver score
+                case
+                  -- next receiver point score is higher --> receiver won point
+                  when point_score_receiver_int < next_point_point_score_receiver_int then point_receiver
+                  -- current receiver point score is higher --> server won point
+                  -- could be case when receiver score goes from 'AD' to '40'
+                  when point_score_receiver_int > next_point_point_score_receiver_int then point_server
+                  else null
+                end
+              -- next server point is lower --> receiver won point
+              -- could be case when server score goes from 'AD' to '40'
+              when point_score_server_int > next_point_point_score_server_int then point_receiver
+              else null -- may be unnecessary since WHEN statements cover all scenarios
+            end
+          -- next point server is NOT same as current point server
+          -- may be case in a tiebreaker
+          when point_server != next_point_point_server then
+            -- server becomes next point receiver --> compare current point server score and next point receiver score
+            case
+              -- next receiver point score is higher --> server won point
+              when point_score_server_int < next_point_point_score_receiver_int then point_server
+              -- next receiver point score is same --> server lost point and score remaind unchanged
+              when point_score_server_int = next_point_point_score_receiver_int then point_receiver
+              else null
+            end
+          else null -- may be unnecessary since WHEN statements cover all scenarios
+        end 
+      -- next point is in next game --> compare 'game' and maybe 'set' level data
+      when game_number_in_match < next_point_game_number_in_match then
+        -- compare current game server to next game server
+        case
+          -- current game server is next game server - scenario shouldn't exist due to 'alternating server' format in tennis
+          -- MAYBE there's a tournament in which a server could server subsequent games, perhaps a service game followed by a tiebreak?
+          when point_server = next_point_point_server then
+            -- compare server game score to next server game score
+            case
+              -- next server game score is higher --> server won game
+              when game_score_server < next_point_game_score_server then point_server
+              -- server game score remains unchanged --> receiver won game
+              when game_score_server = next_point_game_score_server then point_receiver
+              -- server game score is greater then next game score
+              -- could be case if start of a new set
+              when game_score_server > next_point_game_score_server then
+                -- compare current server set score and next server set score
+                case
+                  -- next server set score is higher --> server won
+                  when set_score_server < next_point_set_score_server then point_server
+                  -- server set score remains unchanged --> receiver won game
+                  when set_score_server = next_point_set_score_server then point_receiver
+                  else null
+                end
+              else null -- may be unnecessary since WHEN statements cover all scenarios
+            end
+          -- current game server is NOT next game server - should be the case due to 'alternating server' format
+          -- server becomes next point receiver
+          when point_server != next_point_point_server then
+            -- compare current server game score to next game receiver score
+            case
+              -- next receiver game score is higher --> server won game
+              when game_score_server < next_point_game_score_receiver then point_server
+              -- server game score remains unchanged --> receiver won game
+              when game_score_server = next_point_game_score_receiver then point_receiver
+              -- server game score is greater then next game score
+              -- could be case if start of a new set
+              when game_score_server > next_point_game_score_receiver then
+                -- compare set scores of current server and next point receiver
+                case
+                  -- next point server set score higher --> server won
+                  when set_score_server < next_point_set_score_receiver then point_server
+                  -- server set score remains unchanged --> receiver won game
+                  when set_score_server = next_point_set_score_receiver then point_receiver
+                  else null
+                end
+              else null -- may be unnecessary since WHEN statements cover all scenarios
+            end
+          else null -- may be unnecessary since WHEN statements cover all scenarios
+        end
+      else null
+    end
+    as point_winner_next_point
+
+  from match_points_lead
+),
+
+-- determine point loser based on next point
+match_points_point_next_loser_point as (
+  select
+    *,
+    case
+      when point_winner_next_point = point_server then point_receiver
+      when point_winner_next_point != point_server then point_server
+      else null
+    end as point_loser_next_point
+  from match_points_point_next_winner_point
+),
+
+-------------------------------------------------------
+-- end of section
 
 final as (
   select
@@ -192,8 +355,10 @@ final as (
     rally_length,
 
     point_result,
-    point_winner,
-    point_loser,
+    point_winner_next_point,
+    point_loser_next_point,
+    point_winner_rally,
+    point_loser_rally,
 
     point_score_in_game,
     point_score_server,
@@ -201,18 +366,18 @@ final as (
     point_number_in_set,
     point_number_in_game,
 
-    set_score_in_match,
-    set_score_server,
-    set_score_receiver,
-    set_number_in_match,
-    
     game_score_in_set,
     game_score_server,
     game_score_receiver,
     game_number_in_match,
-    game_number_in_set
+    game_number_in_set,
+
+    set_score_in_match,
+    set_score_server,
+    set_score_receiver,
+    set_number_in_match
     
-  from match_points_loser
+  from match_points_point_next_loser_point
 )
 
 select * from final
