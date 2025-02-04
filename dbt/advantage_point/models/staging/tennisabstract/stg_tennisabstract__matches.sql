4e0517c37b2788d3345f19c4c17f45d5f6d7d528
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-04 17:35:18 -0500

    Revise stg_tennisabstract__matches

--- dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__matches.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg_tennisabstract__matches.sql
@@ -9,16 +9,14 @@ source as (
 renamed as (
     select
         match_url,
-        match_date, -- remains string, will be cast in intermediate layer
+        cast(match_date as date) as match_date,
         match_gender,
         replace(match_tournament, '_', ' ') as match_tournament,
         match_round,
         replace(match_player_one, '_', ' ') as match_player_one,
         replace(match_player_two, '_', ' ') as match_player_two,
         match_title,
-        match_result,
-        audit_field_active_flag as is_record_active,
-        audit_field_start_datetime_utc as loaded_at
+        match_result
     from source
 )
 
