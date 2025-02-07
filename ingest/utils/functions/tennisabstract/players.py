2b15b5126a47ecfbe692f6ff58eecd3c73c6fff6
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-07 12:57:30 -0500

    Create function to scrape player data without driver

--- ingest/utils/functions/tennisabstract/players.py
+++ ingest/utils/functions/tennisabstract/players.py
@@ -72,15 +72,82 @@ def get_player_list() -> List[Dict]:
 
     return player_data_list
 
-def get_player_data_scraped(
-    driver: webdriver,
+# def get_player_data_scraped(
+#     driver: webdriver,
+#     player_url: str,
+#     retries: int,
+#     delay: int
+# ) -> Dict:
+#     """
+#     Arguments:
+#     - driver: Selenium webdriver
+#     - player_url: player link
+#     - retries: Number of retry attempts
+#     - delay: Time (in seconds) between retries
+
+#     Returns dictionary of player information from url
+#     """
+
+#     # initialize data
+#     # initialize data to be retrieved
+#     response_var_list = ['nameparam', 'fullname', 'lastname', 'currentrank', 'peakrank', 'peakfirst', 'peaklast', 'dob', 'ht', 'hand', 'backhand', 'country', 'shortlist', 'careerjs', 'active', 'lastdate', 'twitter', 'current_dubs', 'peak_dubs', 'peakfirst_dubs', 'liverank', 'chartagg', 'photog', 'photog_credit', 'photog_link', 'itf_id', 'atp_id', 'dc_id', 'wiki_id', 'elo_rating', 'elo_rank',]
+#     player_dict = {var: None for var in response_var_list}
+
+#     attempt = 0
+
+#     while attempt < retries:
+
+#         try:
+
+#             # navigate to the page
+#             driver.get(player_url)
+
+#             # wait for the page to fully render (ensure JavaScript is executed)
+#             WebDriverWait(driver, 10).until(
+#                 EC.presence_of_element_located((By.XPATH, "//script[@language='JavaScript']"))
+#             )
+#             # locate script tag
+#             script_tag = driver.find_element(By.XPATH, "//script[@language='JavaScript']")
+#             script_content = script_tag.get_attribute("innerHTML")
+
+#             for var in response_var_list:
+#                 try:
+#                     val = scrape_javascript_var(
+#                         content=script_content,
+#                         var=var
+#                     )
+#                     player_dict[var] = val
+#                 except Exception as e:
+#                     logging.info(f"Error encountered when getting data for variable {var}: {e}")
+
+#             # check if all values in dict are None -> return empty dict
+#             if all(value is None for value in player_dict.values()):
+#                 logging.info(f"All values None for {player_url} - Returning empty dictionary.")
+#                 return {}
+
+#             # return dictionary if data successfully extracted
+#             return player_dict
+
+#         except Exception as e:
+#             attempt += 1
+#             logging.warning(f"Attempt {attempt} failed for {player_url}: {e}")
+#             if attempt < retries:
+#                 logging.info(f"Retrying in {delay} seconds...")
+#                 time.sleep(delay)  # Delay before retrying
+#             else:
+#                 logging.error(f"Max retries reached for {player_url}.")
+
+#     # Return empty dictionary if all retries fail
+#     logging.info(f"Returning empty dictionary")
+#     return {}
+
+def scrape_player_data(
     player_url: str,
     retries: int,
     delay: int
 ) -> Dict:
     """
     Arguments:
-    - driver: Selenium webdriver
     - player_url: player link
     - retries: Number of retry attempts
     - delay: Time (in seconds) between retries
@@ -88,10 +155,10 @@ def get_player_data_scraped(
     Returns dictionary of player information from url
     """
 
-    # initialize data
     # initialize data to be retrieved
     response_var_list = ['nameparam', 'fullname', 'lastname', 'currentrank', 'peakrank', 'peakfirst', 'peaklast', 'dob', 'ht', 'hand', 'backhand', 'country', 'shortlist', 'careerjs', 'active', 'lastdate', 'twitter', 'current_dubs', 'peak_dubs', 'peakfirst_dubs', 'liverank', 'chartagg', 'photog', 'photog_credit', 'photog_link', 'itf_id', 'atp_id', 'dc_id', 'wiki_id', 'elo_rating', 'elo_rank',]
     player_dict = {var: None for var in response_var_list}
+
 
     attempt = 0
 
@@ -100,20 +167,13 @@ def get_player_data_scraped(
         try:
 
             # navigate to the page
-            driver.get(player_url)
-
-            # wait for the page to fully render (ensure JavaScript is executed)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//script[@language='JavaScript']"))
-            )
-            # locate script tag
-            script_tag = driver.find_element(By.XPATH, "//script[@language='JavaScript']")
-            script_content = script_tag.get_attribute("innerHTML")
-
+            response = make_request(url=player_url)
+            response_text = response.text
+                
             for var in response_var_list:
                 try:
                     val = scrape_javascript_var(
-                        content=script_content,
+                        content=response_text,
                         var=var
                     )
                     player_dict[var] = val
