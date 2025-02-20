ce41de8d97c7d43c5487bee436bac2c3b7bda573
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-20 00:55:01 -0500

    Remove intermeidate macro to generate surrogate keys

--- dbt/advantage_point/macros/generate_tournament_surrogate_key.sql
+++ dbt/advantage_point/macros/generate_tournament_surrogate_key.sql
@@ -1,3 +1,3 @@
 {% macro generate_tournament_surrogate_key(tournament_year_col, tournament_gender_col, tournament_name_col) %}
-    {{ generate_surrogate_key(tournament_year_col, tournament_gender_col, tournament_name_col) }}
+    {{ dbt_utils.generate_surrogate_key([tournament_year_col, tournament_gender_col, tournament_name_col]) }}
 {% endmacro %}
