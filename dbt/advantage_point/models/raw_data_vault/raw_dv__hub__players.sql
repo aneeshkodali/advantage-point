5fc600b0da284bc46eaa9b94acd420561362911e
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-20 00:17:05 -0500

    Create raw_dv__hub__players model

---
+++ dbt/advantage_point/models/raw_data_vault/raw_dv__hub__players.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        unique_key='hk_player'
+    )
+}}
+
+with
+
+tennisabstract_players as (
+    select
+        {{ generate_player_surrogate_key('player_name', 'player_gender') }} as hk_player,
+        player_name,
+        player_gender,
+        current_timestamp as load_dts,
+        'tennisabstract' as record_source
+    from {{ ref('stg__tennisabstract__players')}}
+)
+
+select * from tennisabstract_players
