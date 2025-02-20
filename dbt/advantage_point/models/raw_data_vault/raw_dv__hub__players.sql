78669889edd3a3a5789b87ecb49009dbf3eae90b
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-20 00:32:07 -0500

    Add space before curly brace

--- dbt/advantage_point/models/raw_data_vault/raw_dv__hub__players.sql
+++ dbt/advantage_point/models/raw_data_vault/raw_dv__hub__players.sql
@@ -16,7 +16,7 @@ tennisabstract_players as (
         player_gender,
         current_timestamp as load_dts,
         'tennisabstract' as record_source
-    from {{ ref('stg__tennisabstract__players')}}
+    from {{ ref('stg__tennisabstract__players') }}
 )
 
 select * from tennisabstract_players
