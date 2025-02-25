9804997a212c1b2c11176e3805a59d7684c53aa4
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-25 13:49:41 -0500

    Rename player id fields

--- dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__players.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__players.sql
@@ -11,7 +11,7 @@ renamed as (
         player_gender,
         {{ remove_empty_string_from_source('photog_credit') }} as photog_credit,
         {{ convert_rank_to_integer('current_dubs') }} as current_dubs,
-        {{ remove_empty_string_from_source('atp_id') }} as atp_id,
+        {{ remove_empty_string_from_source('atp_id') }} as player_tour_id,
         {{ remove_empty_string_from_source('photog_link') }} as photog_link,
         {{ remove_empty_string_from_source('fullname') }} as player_full_name,
         {{ remove_empty_string_from_source('twitter') }} as twitter,
@@ -33,12 +33,12 @@ renamed as (
         {{ remove_empty_string_from_source('country') }} as player_country,
         {{ remove_empty_string_from_source('backhand') }} as player_backhand,
         to_date(peaklast, 'YYYYMMDD') as peaklast,
-        {{ remove_empty_string_from_source('dc_id') }} as dc_id,
+        {{ remove_empty_string_from_source('dc_id') }} as player_team_cup_id,
         {{ remove_empty_string_from_source('lastname') }} as player_last_name,
         to_date(dob, 'YYYYMMDD') as player_date_of_birth,
         cast({{ remove_empty_string_from_source('ht') }} as int) as player_height_in_cm,
         {{ convert_rank_to_integer('peakrank') }} as peakrank,
-        {{ remove_empty_string_from_source('itf_id') }} as itf_id,
+        {{ remove_empty_string_from_source('itf_id') }} as player_itf_id,
         {{ remove_empty_string_from_source('wiki_id') }} as wiki_id
     from source
 )
