170f8336422ef5ab71dbeb7d319484112fcd56ad
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-25 07:25:24 -0500

    Redo raw_dv__hub__players model

--- dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__players.sql
+++ dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__players.sql
@@ -8,6 +8,24 @@ with
 
 tennisabstract_players as (
     select
+        *,
+        'tennisabstract' as record_source
+    from {{ ref('stg__tennisabstract__players') }}
+),
+
+-- union data
+players_union as (
+    (
+        select
+            player_name,
+            player_gender,
+            record_source
+        from tennisabstract_players
+    )
+),
+
+final as (
+    select
         {{ generate_player_surrogate_key(
             player_name_col='player_name',
             player_gender_col='player_gender'
@@ -15,8 +33,11 @@ tennisabstract_players as (
         player_name,
         player_gender,
         current_timestamp as load_datetime,
-        'tennisabstract' as record_source
-    from {{ ref('stg__tennisabstract__players') }}
+        record_source
+    from players_union
+    {% if is_incremental() %}
+    where hk_player not in (select hk_player from {{ this }})
+    {% endif %}
 )
 
-select * from tennisabstract_players
+select * from final
