42ee58938fa6d3a52fcfc978dea98ab5cf5ee15a
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-26 12:14:37 -0500

    Format hub models

--- dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__players.sql
+++ dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__players.sql
@@ -7,29 +7,30 @@
 with
 
 tennisabstract_players as (
-    select
-        *,
-        'tennisabstract' as record_source
-    from {{ ref('stg__tennisabstract__players') }}
+    select * from {{ ref('stg__tennisabstract__players') }}
 ),
 
 -- union data
 players_union as (
     select
+        hk_player,
+
         player_name,
         player_gender,
+
         record_source,
-        hk_player,
         row_number() over (partition by hk_player order by record_source) as rn -- assing row number
     from (
         select
-            player_name,
-            player_gender,
             {{ generate_player_surrogate_key(
                 player_name_col='player_name',
                 player_gender_col='player_gender'
             ) }} as hk_player,
-            record_source
+
+            player_name,
+            player_gender,
+
+            'tennisabstract' as record_source
         from tennisabstract_players
     ) as p_union
 ),
@@ -37,8 +38,10 @@ players_union as (
 final as (
     select
         hk_player,
+
         player_name,
         player_gender,
+        
         current_timestamp as load_datetime,
         record_source
     from players_union
--- dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__tournaments.sql
+++ dbt/advantage_point/models/raw_data_vault/hub/raw_dv__hub__tournaments.sql
@@ -7,32 +7,33 @@
 with
 
 tennisabstract_tournaments as (
-    select
-        *,
-        'tennisabstract' as record_source
-    from {{ ref('stg__tennisabstract__tournaments') }}
+    select * from {{ ref('stg__tennisabstract__tournaments') }}
 ),
 
 -- union data
 tournaments_union as (
     select
+        hk_tournament,
+
         tournament_year,
         tournament_gender,
         tournament_name,
+
         record_source,
-        hk_tournament,
         row_number() over (partition by hk_tournament order by record_source) as rn -- assing row number
     from (
         select
-            tournament_year,
-            tournament_gender,
-            tournament_name,
             {{ generate_tournament_surrogate_key(
                 tournament_year_col='tournament_year',
                 tournament_gender_col='tournament_gender',
                 tournament_name_col='tournament_name'
             ) }} as hk_tournament,
-            record_source
+
+            tournament_year,
+            tournament_gender,
+            tournament_name,
+            
+            'tennisabstract' as record_source
         from tennisabstract_tournaments
     ) as t_union
 ),
@@ -40,9 +41,11 @@ tournaments_union as (
 final as (
     select
         hk_tournament,
+
         tournament_year,
         tournament_gender,
         tournament_name,
+        
         current_timestamp as load_datetime,
         record_source
     from tournaments_union
