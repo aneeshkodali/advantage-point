0ffea3e3039e8e36b7b086acfb20726fe320b347
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-27 22:37:10 -0500

    Redo script

--- ingest/ingest_tennisabstract_match_points.py
+++ ingest/ingest_tennisabstract_match_points.py
@@ -1,12 +1,10 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from ingest.utils.functions.sql import (
     create_connection,
-    get_table_column_list,
-    ingest_df_to_sql,
+    load_df_to_sql,
 )
-from ingest.utils.functions.tennisabstract import (
+from ingest.utils.functions.tennisabstract.matches import (
+    get_match_point_data_scraped,
     get_match_url_list as get_match_url_list_tennisabstract,
-    get_match_point_data,
 )
 import logging
 import os
@@ -20,93 +18,41 @@ def main():
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-    # set constants for use in function
+    # set constants
     target_schema_name = os.getenv('SCHEMA_INGESTION')
-    temp_schema_name = os.getenv('SCHEMA_INGESTION_TEMP')
     target_table_name = 'tennisabstract_match_points'
-    temp_table_name = target_table_name
-    unique_column_list = ['match_url', 'point_number',]
-    alter_table_drop_column_flag = False
-    merge_table_delete_row_flag = False
 
-    # create connection
-    conn = create_connection()
+    # get list of match urls from source
+    match_url_list = get_match_url_list_tennisabstract()
 
-    # get list of matches
-    match_url_list_tennisabstract = get_match_url_list_tennisabstract()
-    match_url_list_db = get_table_column_list(
-        connection=conn,
-        schema_name=target_schema_name,
-        table_name=target_table_name,
-        column_name_list=['match_url',],
-        where_clause_list=['audit_field_active_flag = TRUE']
-    )
-    match_url_list = list(
-        filter(
-            lambda url_dict: url_dict not in match_url_list_db, match_url_list_tennisabstract
-        )
-    )[:100]
-    logging.info(f"Found {len(match_url_list)} matches.")
+    # loop through match urls
+    for i, match_url_dict in enumerate(match_url_list):
 
-    # loop through matches
-    # initialize chunk logic
-    i = 0
-    chunk_size = 10
-    max_workers = 5
-    for i in range(0, len(match_url_list), chunk_size):
+        match_url = match_url_dict['match_url']
+        logging.info(f"({i+1}/{len(match_url_list)}) Getting match point data for match url: {match_url}")
 
-        match_url_chunk_list = match_url_list[i:i + chunk_size]
-
-        chunk_size_start = i
-        chunk_size_end = min(i + chunk_size, len(match_url_list))
-        logging.info(f"Chunking: {chunk_size_start} to {chunk_size_end}")
-
-        # initialize data list
-        match_point_data_list_master = []
-
-        # Use ThreadPoolExecutor to scrape match data in parallel
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            # Submit tasks directly to executor
-            future_to_url = {
-                executor.submit(get_match_point_data, match_url_dict['match_url'], retries=3, delay=3): idx
-                for idx, match_url_dict in enumerate(match_url_chunk_list, start=chunk_size_start)
-            }
-
-            # Process results as they complete
-            for future in as_completed(future_to_url):
-                url_index = future_to_url[future]  # Get the original index of the URL
-                match_url_dict = match_url_chunk_list[url_index - chunk_size_start]
-                try:
-                    result = future.result()  # Get the result of `get_match_point_data`
-                    if result:
-                        match_point_data_list_master.extend(result)
-                        logging.info(
-                            f"Successfully fetched data for URL {url_index + 1}/{len(match_url_list)}: {match_url_dict['match_url']}"
-                        )
-                except Exception as e:
-                    logging.info(
-                        f"Failed to fetch data for URL {url_index + 1}/{len(match_url_list)}: {match_url_dict['match_url']} - Error: {e}"
-                    )
-        
-        # create dataframe
-        match_point_data_df = pd.DataFrame(match_point_data_list_master)
-
-        # ingest dataframe to sql
-        ingest_df_to_sql(
-            connection=conn,
-            df=match_point_data_df,
-            target_schema_name=target_schema_name,
-            target_table_name=target_table_name,
-            temp_schema_name=temp_schema_name,
-            temp_table_name=temp_table_name,
-            unique_column_list=unique_column_list,
-            drop_column_flag=alter_table_drop_column_flag,
-            delete_row_flag=merge_table_delete_row_flag
+        # get data from match point scraping
+        match_point_scraped_list = get_match_point_data_scraped(
+            match_url=match_url,
+            retries=3,
+            delay=3
         )
 
-    # close connection
-    conn.close()
+        # continue with match point data logic if data is returned from scraping
+        if match_point_scraped_list != []:
 
+            logging.info(f"Match point data found for match url: {match_url}")
+
+            # load data to database
+            match_point_data_df = pd.DataFrame(match_point_scraped_list) # create dataframe
+            conn = create_connection() # create connection
+            load_df_to_sql(
+                connection=conn,
+                df=match_point_data_df,
+                target_schema_name=target_schema_name,
+                target_table_name=target_table_name,
+            )
+            conn.close() # close connection
 
 if __name__ == "__main__":
     main()
