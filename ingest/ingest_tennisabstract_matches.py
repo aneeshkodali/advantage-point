52c0ee6dc8d8d246590c252b62bca936d8adefd4
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-02-28 07:06:15 -0500

    Remove subset

--- ingest/ingest_tennisabstract_matches.py
+++ ingest/ingest_tennisabstract_matches.py
@@ -36,7 +36,7 @@ def main():
     temp_table_name = target_table_name
 
     # get list of match urls from source
-    match_url_list = get_match_url_list_tennisabstract()[:5]
+    match_url_list = get_match_url_list_tennisabstract()
 
     # loop through match urls
     match_data_list = []
