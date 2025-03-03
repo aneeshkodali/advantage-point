b3009d5f58602ffe9dbc19a583a371285d5f4c10
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-03-03 09:03:10 -0500

    Add raw_dv__sat__player_media_presence model

---
+++ dbt/advantage_point/models/raw_data_vault/sat/raw_dv__sat__player_media_presence.sql
@@ -0,0 +1,65 @@
+{{
+    config(
+        unique_key=['hk_player', 'load_datetime']
+    )
+}}
+
+with
+
+tennisabstract_players as (
+    select
+        player_name,
+        player_gender,
+
+        player_twitter_handle,
+        player_wikipedia_id,
+
+        'tennisabstract__players' as record_source
+    from {{ ref('stg__tennisabstract__players') }}
+),
+
+-- union data
+records_union as (
+    (select * from tennisabstract_players)
+),
+
+-- create hashes (surrogate key, hash diff)
+records_hash as (
+    select
+        *,
+        {{ generate_player_surrogate_key(
+            player_name_col='player_name',
+            player_gender_col='player_gender'
+        ) }} as hk_player,
+        {{ dbt_utils.generate_surrogate_key([
+            'player_twitter_handle',
+            'player_wikipedia_id',
+        ]) }} as hash_diff
+    from records_union
+),
+
+-- filter for incremental changes
+final as (
+    select
+        hk_player,
+
+        player_twitter_handle,
+        player_wikipedia_id,
+
+        hash_diff,
+        current_timestamp as load_datetime,
+        record_source
+    from records_hash
+    where 1=1
+        {% if is_incremental() %}
+        and not exists (
+            select 1
+            from {{ this }} as existing
+            where 1=1
+                and existing.hk_player = final.hk_player
+                and existing.hash_diff = final.hash_diff
+        )
+        {% endif %} 
+)
+
+select * from final
