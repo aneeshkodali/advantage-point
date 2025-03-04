79ba18b17eeaf58f7bca8e6d7414ae685e16dbf5
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-03-04 04:15:17 -0500

    Format macros

--- dbt/advantage_point/macros/generate_player_surrogate_key.sql
+++ dbt/advantage_point/macros/generate_player_surrogate_key.sql
@@ -1,3 +1,11 @@
-{% macro generate_player_surrogate_key(player_name_col, player_gender_col) %}
-    {{ dbt_utils.generate_surrogate_key([player_name_col, player_gender_col]) }}
+{% macro generate_player_surrogate_key(
+    player_name_col,
+    player_gender_col
+) %}
+    {{ dbt_utils.generate_surrogate_key(
+        [
+            player_name_col,
+            player_gender_col
+        ]
+    ) }}
 {% endmacro %}
