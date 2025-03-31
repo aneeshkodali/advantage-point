1cff4760ecca863a18bd837b9708a31555f9a53d
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-03-30 23:58:10 -0400

    Create match business key macro

---
+++ dbt/advantage_point/macros/business_key/generate_match_business_key.sql
@@ -0,0 +1,20 @@
+{% macro generate_match_business_key(
+    match_date_col,
+    match_gender_col,
+    match_tournament_col,
+    match_round_col,
+    match_player_array_col
+) %}
+
+    lower(
+        concat_ws(
+            '||',
+            cast(match_date_col as string),
+            match_gender_col,
+            match_tournament_col,
+            match_round_col,
+            cast(match_player_array_col as string)
+        )
+    )
+    
+{% endmacro %}
