587b5b085254b53572c6bc6368622841b40ab7c0
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-03-31 08:30:45 -0400

    Add logic to date fields to remove empty strings

--- dbt/advantage_point/macros/remove_empty_string_from_source.sql
+++ dbt/advantage_point/macros/remove_empty_string_from_source.sql
@@ -1,6 +1,7 @@
 {% macro remove_empty_string_from_source(column_name) %}
     case
-        when replace({{ column_name }}, '''', '') = '' then null
+        when {{ column_name }} = '''""''' then null
+        when {{ column_name }} = '''''' then null
         else replace({{ column_name }}, '''', '')
     end
 {% endmacro %}
--- dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__players.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__players.sql
@@ -16,7 +16,10 @@ renamed as (
         {{ remove_empty_string_from_source('fullname') }} as player_full_name,
         {{ remove_empty_string_from_source('twitter') }} as player_twitter_handle,
         {{ remove_empty_string_from_source('photog') }} as photog,
-        to_date(peakfirst, 'YYYYMMDD') as player_first_peak_singles_ranking_date,
+        to_date(
+            {{ remove_empty_string_from_source('peakfirst') }},
+            'YYYYMMDD'
+        ) as player_first_peak_singles_ranking_date,
         chartagg,
         cast(cast(active as int) as boolean) as is_player_active,
         nameparam,
@@ -24,11 +27,10 @@ renamed as (
         shortlist,
         careerjs,
         {{ convert_rank_to_integer('peak_dubs') }} as player_peak_doubles_ranking,
-        case peakfirst_dubs
-            when '''""''' then null
-            when '''''' then null
-            else to_date(peakfirst_dubs, 'YYYYMMDD')
-        end as player_first_peak_doubles_ranking_date,
+        to_date(
+            {{ remove_empty_string_from_source('peakfirst_dubs') }},
+            'YYYYMMDD'
+        ) as player_first_peak_doubles_ranking_date,
         {{ remove_empty_string_from_source('hand') }} as player_hand,
         {{ convert_rank_to_integer('currentrank') }} as player_current_singles_rank,
         {{ remove_empty_string_from_source('country') }} as player_country,
@@ -36,18 +38,21 @@ renamed as (
         to_date(peaklast, 'YYYYMMDD') as player_last_peak_singles_ranking_date,
         {{ remove_empty_string_from_source('dc_id') }} as player_team_cup_id,
         {{ remove_empty_string_from_source('lastname') }} as player_last_name,
-        case dob
-            when '''''' then null
-            else to_date(dob, 'YYYYMMDD')
-        end as player_date_of_birth,
+        to_date(
+            {{ remove_empty_string_from_source('dob') }},
+            'YYYYMMDD'
+        ) as player_date_of_birth,
         cast({{ remove_empty_string_from_source('ht') }} as int) as player_height_in_cm,
         {{ convert_rank_to_integer('peakrank') }} as player_peak_singles_ranking,
         {{ remove_empty_string_from_source('itf_id') }} as player_itf_id,
         {{ remove_empty_string_from_source('wiki_id') }} as player_wikipedia_id,
-        case lastdate
-            when '0' then null
-            else to_date(lastdate, 'YYYYMMDD')
-        end as player_last_match_played_date
+        to_date(
+            case lastdate
+                when '0' then '23991231'
+                else {{ remove_empty_string_from_source('lastdate') }}
+            end,
+            'YYYYMMDD'
+        ) as player_last_match_played_date
     from source
 )
 
