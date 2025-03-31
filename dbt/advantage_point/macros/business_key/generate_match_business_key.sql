8a50e0cb9708e75e7ec250cbccdb57549048cf0d
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-03-31 00:13:51 -0400

    Adjust function for business key

--- dbt/advantage_point/macros/business_key/generate_match_business_key.sql
+++ dbt/advantage_point/macros/business_key/generate_match_business_key.sql
@@ -6,15 +6,13 @@
     match_player_array_col
 ) %}
 
-    lower(
-        concat_ws(
-            '||',
-            cast({{ match_date_col }} as string),
-            {{ match_gender_col }},
-            replace({{ match_tournament_col }}, ' ', ''),
-            replace({{ match_round_col }}, ' ', ''),
-            cast(replace({{ match_player_array_col }}, ' ', '') as string)
-        )
+    concat_ws(
+        '||',
+        cast({{ match_date_col }} as string),
+        {{ match_gender_col }},
+        {{ match_tournament_col }},
+        {{ match_round_col }},
+        cast({{ match_player_array_col }} as string)
     )
     
 {% endmacro %}
