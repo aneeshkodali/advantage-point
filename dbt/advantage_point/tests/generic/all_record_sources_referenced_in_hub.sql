d66879df115806024af7c20f2545d8e76dbde891
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-02 01:43:28 -0400

    Fix custom generic test model reference

--- dbt/advantage_point/tests/generic/all_record_sources_referenced_in_hub.sql
+++ dbt/advantage_point/tests/generic/all_record_sources_referenced_in_hub.sql
@@ -1,11 +1,11 @@
-{% test all_record_sources_referenced_in_hub(hub_model) %}
+{% test all_record_sources_referenced_in_hub(model) %}
 
 with
 
 hub_record_sources as (
     select distinct
         record_source
-    from {{ hub_model }}
+    from {{ model }}
 ),
 
 stg_record_sources as (
@@ -22,7 +22,7 @@ missing_record_sources as (
     left join stg_record_sources as stg_rec_src on 1=1
         and hub_rec_src.record_source = stg_rec_src.record_source
     where 1=1
-        and stg_rec_src.hub_name = '{{ hub_model.identifier }}'
+        and stg_rec_src.hub_name = '{{ model.identifier }}'
         and stg_rec_src.record_source is null
 )
 
