9f4fc041514472aa00a802a0ea07b28def410662
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-08 08:44:42 -0400

    Add match point business key

---
+++ dbt/advantage_point/macros/business_key/generate_match_point_business_key.sql
@@ -0,0 +1,20 @@
+{% macro generate_match_business_key(
+    match_date_col,
+    match_gender_col,
+    match_tournament_col,
+    match_round_col,
+    match_players_col,
+    match_point_col,
+) %}
+
+    concat_ws(
+        '||',
+        cast({{ match_date_col }} as text),
+        {{ match_gender_col }},
+        {{ match_tournament_col }},
+        {{ match_round_col }},
+        {{ match_players_col }},
+        lpad(cast({{ match_point_col }} as text), 4, '0')
+    )
+    
+{% endmacro %}
--- dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__match_points.sql
+++ dbt/advantage_point/models/staging/tennisabstract/stg__tennisabstract__match_points.sql
@@ -4,6 +4,10 @@ source as (
     select * from {{ source('tennisabstract', 'match_points') }}
 ),
 
+matches as (
+    select * from {{ ref('stg__tennisabstract__matches') }}
+),
+
 /*
 note:
 '‑' is a non-breaking hyphen
@@ -19,6 +23,24 @@ renamed as (
         replace(points, '‑', '-') as point_score_in_game,
         point_description
     from source
+),
+
+-- create business key
+bk as (
+    select
+        mp.*,
+        {{ generate_match_point_business_key(
+            match_date_col='m.match_date',
+            match_gender_col='m.match_gender',
+            match_tournament_col='m.match_tournament',
+            match_round_col='m.match_round',
+            match_players_col='m.match_players',
+            match_point_col='mp.point_number_in_match'
+        ) }} as bk_match_point,
+        m.bk_match,
+        'tennisabstract__match_points' as record_source
+    from renamed as mp
+    left join matches as m on mp.match_url = m.match_url
 )
 
-select * from renamed
+select * from bk
