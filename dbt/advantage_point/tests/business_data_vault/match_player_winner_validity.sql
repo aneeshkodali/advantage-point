e344f65b07df4b3082c91f8d23e7ef59f3208968
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-14 00:10:34 -0400

    Add test for valid match player results

---
+++ dbt/advantage_point/tests/business_data_vault/match_player_winner_validity.sql
@@ -0,0 +1,16 @@
+-- tests/business_vault/test_match_player_winner_validity.sql
+
+with
+
+match_agg as (
+    select
+        hk_match,
+        count(*) as player_count,
+        sum(cast(is_match_winner as int)) as winner_count
+    from {{ ref('bus_dv__bridge__match__player') }}
+    group by 1
+)
+
+select *
+from match_agg
+where (player_count != 2 or winner_count != 1)
