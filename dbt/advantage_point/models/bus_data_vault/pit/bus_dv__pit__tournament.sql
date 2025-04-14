8061dcd8966f374448e47a8f851bf73ef1544574
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-13 23:45:09 -0400

    Add bus_dv__pit__tournament model

--- dbt/advantage_point/models/bus_data_vault/pit/bus_dv__pit__tournament.sql
+++ dbt/advantage_point/models/bus_data_vault/pit/bus_dv__pit__tournament.sql
@@ -9,17 +9,26 @@ tournament_details as (
     ) }}
 ),
 
+-- get unique list of hk
+hk_union as (
+    (select hk_tournament from tournament_details)
+),
+
+
 final as (
     select
-        hk_tournament,
-        tournament_start_date,
-        tournament_surface,
-        tournament_draw_size,
-        hash_diff,
-        load_datetime as sat_load_datetime,
+        hk_union.hk_tournament,
+
+        tournament_details.tournament_start_date,
+        tournament_details.tournament_surface,
+        tournament_details.tournament_draw_size,
+        tournament_details.hash_diff as tournament_details_hash_diff,
+        tournament_details.load_datetime as tournament_details._load_datetime,
+
         current_timestamp as bus_dv_load_datetime,
         'bus_dv__pit__tournament' as bus_dv_source_model
-    from match_details
+    from hk_union
+    left join tournament_details using (hk_tournament)
 )
 
 select * from final
