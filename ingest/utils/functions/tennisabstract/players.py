ccf15775ec456d3b8d1f8c78e25dea61f87ed3d3
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-15 09:48:52 -0400

    Change wait function logic

--- ingest/utils/functions/tennisabstract/players.py
+++ ingest/utils/functions/tennisabstract/players.py
@@ -184,7 +184,7 @@ def scrape_player_data_playwright(
                 player_url,
                 wait_until='networkidle'
             )
-            page.wait_for_function(f"window.{response_var_list[0]} !== undefined", timeout=5000)
+            page.wait_for_function("typeof nameparam !== undefined", timeout=5000)
             response_text = page.content()
             response_status_code = response.status if response else None
                 
