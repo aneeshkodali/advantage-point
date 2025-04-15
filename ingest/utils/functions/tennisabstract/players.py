0ea1bee1d63168c81d832fd135796ecc4936ae0f
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-15 09:50:21 -0400

    Add quotes to undefined

--- ingest/utils/functions/tennisabstract/players.py
+++ ingest/utils/functions/tennisabstract/players.py
@@ -184,7 +184,7 @@ def scrape_player_data_playwright(
                 player_url,
                 wait_until='networkidle'
             )
-            page.wait_for_function("typeof nameparam !== undefined", timeout=5000)
+            page.wait_for_function("typeof nameparam !== 'undefined'", timeout=5000)
             response_text = page.content()
             response_status_code = response.status if response else None
                 
