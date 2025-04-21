f6de85bc708db7012537e493283066aae8d0ff11
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-21 13:28:41 -0400

    Add bus_dv__hub__set model

--- dbt/advantage_point/models/bus_data_vault/hub/bus_dv__hub__set.sql
+++ dbt/advantage_point/models/bus_data_vault/hub/bus_dv__hub__set.sql
@@ -10,19 +10,62 @@ hub_record_sources as (
     select * from {{ ref('stg__seed__hub_record_sources') }}
 ),
 
--- get match columns
--- will be used to construct bk and hk
-hub_match as (
-    select
-        hk_match,
-        match_date,
-        match_gender,
-        match_tournament,
-        match_round,
-        match_players
-    from {{ ref('raw_dv__hub__match') }}
+sat_point as (
+    select distinct
+        bk_set,
+        bus_dv_source_model,
+
+        bk_match,
+        set_number_in_match
+    from {{ ref('bus_dv__sat__point') }}
 ),
 
-sat_point as (
+-- union data
+records_union as (
+    (select * from sat_point)
+),
 
+-- add hub key
+records_hkey as (
+    select
+        *,
+        {{ generate_set_surrogate_key(
+            set_business_key_col='bk_set'
+        ) }} as hk_set
+    from records_union
+),
+
+-- add row number to order records
+records_rownum as (
+    select
+        hub.*,
+        row_number() over (partition by hub.hk_set order by hub_rec_src.sort_order) as rn -- assing row number
+    from records_hkey as hub
+    left join hub_record_sources as hub_rec_src on 1=1
+        and hub_rec_src.hub_name = 'bus_dv__hub__set'
+        and hub.record_source = hub_rec_src.record_source
+),
+
+final as (
+    select
+        hk_set,
+        bk_set,
+        current_timestamp as bus_dv_load_datetime,
+        bus_dv_source_model,
+        
+        bk_match,
+        set_number_in_match
+    from records_rownum as incr
+    where 1=1
+        and rn = 1 -- filter for row number
+        {% if is_incremental() %}
+        and not exists (
+            select 1
+            from {{ this }} as existing
+            where 1=1
+                and existing.hk_set = incr.hk_set
+        ) -- filter for new pk records
+        {% endif %}
 )
+
+select * from final
