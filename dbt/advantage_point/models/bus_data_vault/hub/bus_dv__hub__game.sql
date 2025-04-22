deb94e533520e85fb4dc0e4ed35301732dba3d81
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-22 15:07:39 -0400

    Reference new int view

--- dbt/advantage_point/models/bus_data_vault/hub/bus_dv__hub__game.sql
+++ dbt/advantage_point/models/bus_data_vault/hub/bus_dv__hub__game.sql
@@ -10,19 +10,19 @@ hub_record_sources as (
     select * from {{ ref('stg__seed__hub_record_sources') }}
 ),
 
-sat_point as (
-    select distinct
+int_game as (
+    select
         bk_game,
         bus_dv_source_model,
 
         bk_match,
         game_number_in_match
-    from {{ ref('bus_dv__sat__point') }}
+    from {{ ref('bus_dv__int__game') }}
 ),
 
 -- union data
 records_union as (
-    (select * from sat_point)
+    (select * from int_game)
 ),
 
 -- add hub key
