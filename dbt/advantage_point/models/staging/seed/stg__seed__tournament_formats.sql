55dd8c205bf46c78ba0e11e185fdef814a9b6685
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-25 11:25:01 -0400

    Cast tiebreak_points to int

--- dbt/advantage_point/models/staging/seed/stg__seed__tournament_formats.sql
+++ dbt/advantage_point/models/staging/seed/stg__seed__tournament_formats.sql
@@ -14,7 +14,7 @@ renamed as (
         cast(games_per_set as int) as games_per_set,
         tiebreak_type,
         tiebreak_trigger_game,
-        tiebreak_points,
+        cast(tiebreak_points as int) as tiebreak_points,
         cast(final_set_tiebreak as boolean) as final_set_tiebreak,
         final_set_tiebreak_trigger_game,
         cast(is_ad_scoring as boolean) as is_ad_scoring,
