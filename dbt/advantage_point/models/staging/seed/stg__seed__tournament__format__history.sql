8fb05b03f5e5f73239d7edb8caa5329a8532d1b2
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-04-26 00:27:26 -0400

    Remove default year values

--- dbt/advantage_point/models/staging/seed/stg__seed__tournament__format__history.sql
+++ dbt/advantage_point/models/staging/seed/stg__seed__tournament__format__history.sql
@@ -9,8 +9,8 @@ renamed as (
         tournament_name,
         tournament_gender,
         format_id,
-        cast(coalesce(effective_start_year, '1800') as int) as effective_start_year,
-        cast(coalesce(effective_end_year, '3000') as int) as effective_end_year,
+        cast(effective_start_year as int) as effective_start_year,
+        cast(effective_end_year as int) as effective_end_year,
         note
 
     from source
