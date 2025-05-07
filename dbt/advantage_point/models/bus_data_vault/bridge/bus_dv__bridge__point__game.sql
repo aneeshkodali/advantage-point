5f5629a1b337f625f2665e09c8ec717b03c66d8b
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-05-07 05:42:20 -0400

    Refactor bus_dv__bridge__point__game model

--- dbt/advantage_point/models/bus_data_vault/bridge/bus_dv__bridge__point__game.sql
+++ dbt/advantage_point/models/bus_data_vault/bridge/bus_dv__bridge__point__game.sql
@@ -10,20 +10,8 @@ link_record_sources as (
     select * from {{ ref('stg__seed__link_record_sources') }}
 ),
 
-int_point_scores_parsed as (
-    select * from {{ ref('bus_dv__int__point_scores_parsed') }}
-),
-
-int_point_details as (
-    select * from {{ ref('bus_dv__int__point_details') }}
-),
-
-hub_point as (
-    select * from {{ ref('raw_dv__hub__point') }}
-),
-
-hub_game as (
-    select * from {{ ref('bus_dv__hub__game') }}
+int_point_game as (
+    select * from {{ ref('bus_dv__int__point__game') }}
 ),
 
 -- select columns
@@ -31,41 +19,19 @@ hub_game as (
 point_game as (
     select
         {{ generate_point_game_surrogate_key(
-            point_business_key_col='hub_point.bk_point',
-            game_business_key_col='lnk.bk_game'
+            point_business_key_col='bk_point',
+            game_business_key_col='bk_game'
         ) }} as lk_point_game,
         
-        lnk.hk_point,
-        hub_point.bk_point,
-        hub_game.hk_game,
-        lnk.bk_game,
+        hk_point,
+        bk_point,
+        hk_game,
+        bk_game,
 
-        lnk.point_score_in_game,
-        lnk.point_number_in_game,
+        point_number_in_game,
 
-        lnk.bus_dv_source_model as record_source
-    from int_point_scores_parsed as lnk
-    left join hub_point on lnk.hk_point = hub_point.hk_point
-    left join hub_game on lnk.bk_game = hub_game.bk_game
-),
-
--- calculate break/game points
-point_game_is_winning_point as (
-    select
-        point_game.lk_point_game,
-        point_game.hk_point,
-        point_game.bk_point,
-        point_game.point_score_in_game,
-        point_game.point_number_in_game,
-        point_game.hk_game,
-        point_game.bk_game,
-        point_game.record_source,
-        point_game.point_score_in_game,
-        point_game.point_number_in_game,
-        int_point_details.is_game_point,
-        int_point_details.is_break_point
-    from point_game
-    left join int_point_details on point_game.hk_point = int_point_details.hk_point
+        bus_dv_source_model as record_source
+    from int_point_game
 ),
 
 -- add row number to order records
@@ -89,10 +55,7 @@ final as (
         bk_point,
         hk_game,
         bk_game,
-        point_score_in_game,
         point_number_in_game,
-        is_game_point,
-        is_break_point
 
     from records_rownum as incr
     where 1=1
