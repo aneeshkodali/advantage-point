37df200e337fa8075eaf33f933e352f3a1faaffa
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-05-07 02:21:22 -0400

    Refactor bus_dv__pit__point model

--- dbt/advantage_point/models/bus_data_vault/pit/bus_dv__pit__point.sql
+++ dbt/advantage_point/models/bus_data_vault/pit/bus_dv__pit__point.sql
@@ -7,7 +7,10 @@ point_details as (
         hash_diff,
         record_source,
 
-        point_side
+        point_side,
+        point_length,
+        point_result,
+        rally_length
     from (
         {{ get_latest_record(
             model_ref=ref('bus_dv__sat__point_details'),
@@ -17,30 +20,9 @@ point_details as (
     ) as p
 ),
 
-point_rally as (
-    select
-        hk_point,
-        load_datetime,
-        hash_diff,
-        record_source,
-
-        point_length,
-        point_result,
-        rally_length
-    from (
-        {{ get_latest_record(
-            model_ref=ref('bus_dv__sat__point_rally'),
-            partition_by_col='hk_point',
-            order_by_col='load_datetime'
-        ) }}
-    ) as p
-),
-
 -- get unique list of hk
 hk_union as (
     (select hk_point from point_details)
-    union
-    (select hk_point from point_rally)
 ),
 
 final as (
@@ -48,20 +30,16 @@ final as (
         hk_union.hk_point,
 
         point_details.point_side,
+        point_details.point_length,
+        point_details.point_result,
+        point_details.rally_length,
         point_details.hash_diff as point_details_hash_diff,
         point_details.load_datetime as point_details_load_datetime,
-
-        point_rally.point_length,
-        point_rally.point_result,
-        point_rally.rally_length,
-        point_rally.hash_diff as point_rally_hash_diff,
-        point_rally.load_datetime as point_rally_load_datetime,
         
         current_timestamp as bus_dv_load_datetime,
         'bus_dv__pit__point' as bus_dv_source_model
     from hk_union
     left join point_details using (hk_point)
-    left join point_rally using (hk_point)
 )
 
 select * from final
