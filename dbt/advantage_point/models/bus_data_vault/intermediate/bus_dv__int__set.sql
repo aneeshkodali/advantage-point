c9e6580aefb2d276477c1813d58bf8fcbee97165
Aneesh Kodali <aneesh.kodali@gmail.com>
2025-05-06 22:18:54 -0400

    Add columns

--- dbt/advantage_point/models/bus_data_vault/intermediate/bus_dv__int__set.sql
+++ dbt/advantage_point/models/bus_data_vault/intermediate/bus_dv__int__set.sql
@@ -12,14 +12,64 @@ link_match_tournament as (
     select * from {{ ref('raw_dv__link__match__tournament') }}
 ),
 
--- get distinct set rows
-sets_distinct as (
-    select distinct
-        hk_match,
-        bk_match,
-        set_number_in_match,
-        bk_set
+-- get last point in set
+set_last_point as (
+    select
+        bk_set,
+        max(point_number_in_match) as point_number_in_match
     from int_point_scores_parsed
+    group by 1
+),
+
+-- calculate additional set details
+set_details as (
+    select
+        s.*,
+        
+        -- is set deciding set
+        s.set_number_in_match = tfh.best_of_sets as is_deciding_set,
+
+        -- is set a tiebreak set
+        case
+            -- is set deciding/final set
+            when s.set_number_in_match = tfh.best_of_sets then
+                case
+                    -- is tiebreak played in final set
+                    when tf.final_set_tiebreak = true then
+                        case
+                            -- is game score equal to final set tiebreak trigger
+                            when s.game_score_in_set = tfh.final_set_tiebreak_trigger_game then true
+                            when s.game_score_in_set != tfh.final_set_tiebreak_trigger_game then false
+                            else null
+                        end
+                    else null
+                end
+            when s.set_number_in_match != tfh.best_of_sets then
+                case
+                    -- is game score equal to tiebreak trigger
+                    when s.game_score_in_set = tfh.tiebreak_trigger_game then true
+                    when s.game_score_in_set != tfh.tiebreak_trigger_game then false
+                    else null
+                end
+            else null
+        end as is_tiebreak_set
+                    
+
+    from int_point_scores_parsed as s
+    -- join match_tournament to get tournament
+    left join link_match_tournament as m_t on s.hk_match = m_t.hk_match
+    -- join tournament_format_history to get best_of_sets
+    left join tournament_format_history as tfh on m_t.hk_tournament = tfh.hk_tournament
+),
+
+-- filter for last point in set
+set_point_filtered as (
+    select
+        s.*
+    from set_details as s
+    inner join set_max_point as s_m_p on 1=1
+        and s.bk_set = s_m_p.bk_set
+        and s.point_number_in_match = s_m_p.point_number_in_match
 ),
 
 final as (
@@ -28,10 +78,12 @@ final as (
         bk_match,
         set_number_in_match,
         bk_set,
+        is_deciding_set,
+        is_tiebreak_set,
 
         current_timestamp as bus_dv_load_datetime,
         'bus_dv__int__set' as bus_dv_source_model
-    from sets_distinct
+    from set_point_filtered
 )
 
 select * from final
--- dbt/advantage_point/models/bus_data_vault/intermediate/bus_dv__int__set__match.sql
+++ dbt/advantage_point/models/bus_data_vault/intermediate/bus_dv__int__set__match.sql
@@ -4,31 +4,12 @@ int_set as (
     select * from {{ ref('bus_dv__int__set') }}
 ),
 
-tournament_format_history as (
-    select * from {{ ref('bus_dv__int__tournament_format_history') }}
-),
-
-link_match_tournament as (
-    select * from {{ ref('raw_dv__link__match__tournament') }}
-),
-
--- calculate is_deciding_set
-sets_is_deciding_set as (
-    select
-        s.*,
-        s.set_number_in_match = tfh.best_of_sets as is_deciding_set
-    from int_set as s
-    -- join match_tournament to get tournament
-    left join link_match_tournament as m_t on s.hk_match = m_t.hk_match
-    -- join tournament_format_history to get best_of_sets
-    left join tournament_format_history as tfh on m_t.hk_tournament = tfh.hk_tournament
-),
-
 final as (
     select
         hk_match,
         bk_set,
         is_deciding_set,
+        is_tiebreak_set,
 
         current_timestamp as bus_dv_load_datetime,
         'bus_dv__int__set__match' as bus_dv_source_model
